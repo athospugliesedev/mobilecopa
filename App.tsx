215e2f3bd1a3a6362f9e596afb5786ad730885e1
athospugliesedev <athospugliesedev@gmail.com>
2023-02-18 00:02:21 -0300

    pools find page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
+import { Pools } from './src/screens/Pools';
+
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/themes';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+
 
 export default function App() {
+  
+ const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold})
+
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={ THEME }>
+      <AuthContextProvider>
+
+      <StatusBar 
+      style='light'
+      />
+      {
+        fontsLoaded ? <Pools /> : <Loading /> 
+      }
+      </AuthContextProvider>
+
+    </NativeBaseProvider>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
